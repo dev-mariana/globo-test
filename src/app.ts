344bf420b347968e5ecd3862bdee3603f9aa63bd
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2025-09-15 12:33:25 -0300

    chore: add CORS support to the application

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import { connection } from "./config/database/mongo";
 import { createApolloServer } from "./config/graphql";
@@ -5,6 +6,7 @@ import { createApolloServer } from "./config/graphql";
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 connection();
 createApolloServer();
